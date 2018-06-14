4579f8e4167e797747140e89e857026855c5d56a
Gustavo Gimenez <gustavo.gimenez@faea.uncoma.edu.ar>
2018-06-14 15:44:33 +0000

    version 1.5.1

--- R/tucker2R.R
+++ R/tucker2R.R
@@ -15,7 +15,7 @@
 #' @param  amb The diferent conditions, in which the same variables and
 #' individuals had been studied. By default is 2.
 #' @param  stand a boolean value, if it is TRUE (value set by default) each
-#' variabel is centered and scale by variable.
+#' variable is centered and scale by variable.
 #' @param nc1 number of components in the first mode, by default is 2
 #' @param nc2 number of components in the second mode, by default is 2
 #' @param niter the iteration number for the Tuckals algorithm, by default
@@ -27,10 +27,10 @@
 #'  conditions.The labels of the variables must be the same for all conditions
 #'  in the data frame.
 #'
-#' @return \code{Resultado}a list which stores the name of the individual and
-#' the variables,the number of iterations, the variability explained by the
+#' @return \code{Resultado} a list which stores the name of the individual and
+#' the variables, the number of iterations, the variability explained by the
 #' model, and the total variability.
-#' \code{Proyeccion}It is a list which holds the projection of individuals and
+#' \code{Proyeccion} It is a list which holds the projection of individuals and
 #' variables to see if the biplot is difficult to understand because of  the
 #' huge number of cases or plotted vectors.
 #' \code{saltuck} is a list with the results of the algorithm to plot the biplot,
@@ -38,7 +38,7 @@
 #'
 #' @references
 #' \describe{
-#'  \item{MARTICORENA, M.; BRAMARDI, S.; DEFACIO, R. 2010.}{Characterization of maize populations in different environmental
+#'  \item{Marticorena, M.; Bramardi, S.; Defacio, R. 2010.}{Characterization of maize populations in different environmental
 #' conditions by means of Three-Mode Principal Components Analysis. Revista Ciencia e Investigacion   Agraria. 37(3): 93-105.}
 #'  \item{Timmerman, M.E., and H. Kiers. 2000.}{Three-mode principal components analysis. Choosing numbers of components and sensitivity to local
 #' optima. The British Journal of the Mahematical and Statistical Psychology 53: 1-16.}
